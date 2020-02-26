ad52dbd59d750c33c1c5b552e879eeeae2c0b948
Andre Kendeck <ak@vectra.co.za>
2020-02-26 11:34:55 +0200

    Initial Setup.
    - Added Models 📦
    - Added Seeds 🌱
    - Added Emails 📧
    - Added Notifications  🔔
    - Added Migrations 🦜

---
+++ app/Organization.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Laratrust\Models\LaratrustTeam;
+
+class Organization extends LaratrustTeam
+{
+}
---
+++ database/migrations/2020_02_26_092103_laratrust_setup_teams.php
@@ -0,0 +1,79 @@
+<?php
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class LaratrustSetupTeams extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return  void
+     */
+    public function up()
+    {
+        // Create table for storing teams
+        Schema::create('organizations', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name')->unique();
+            $table->string('logo')->nullable();
+            $table->integer('leaves_allowed')->nullable(); 
+            $table->integer('required_approvals')->default(1); 
+            $table->string('display_name')->nullable();
+            $table->string('description')->nullable();
+            $table->timestamps();
+        });
+
+        Schema::table('role_user', function (Blueprint $table) {
+            // Drop role foreign key and primary key
+            $table->dropForeign(['role_id']);
+            $table->dropPrimary(['user_id', 'role_id', 'user_type']);
+
+            // Add team_id column
+            $table->unsignedInteger('team_id')->nullable();
+
+            // Create foreign keys
+            $table->foreign('role_id')->references('id')->on('roles')
+                ->onUpdate('cascade')->onDelete('cascade');
+            $table->foreign('team_id')->references('id')->on('organizations')
+                ->onUpdate('cascade')->onDelete('cascade');
+
+            // Create a unique key
+            $table->unique(['user_id', 'role_id', 'user_type', 'team_id']);
+        });
+
+        Schema::table('permission_user', function (Blueprint $table) {
+           // Drop permission foreign key and primary key
+            $table->dropForeign(['permission_id']);
+            $table->dropPrimary(['permission_id', 'user_id', 'user_type']);
+
+            $table->foreign('permission_id')->references('id')->on('permissions')
+                ->onUpdate('cascade')->onDelete('cascade');
+
+            // Add team_id column
+            $table->unsignedInteger('team_id')->nullable();
+
+            $table->foreign('team_id')->references('id')->on('organizations')
+                ->onUpdate('cascade')->onDelete('cascade');
+
+            $table->unique(['user_id', 'permission_id', 'user_type', 'team_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return  void
+     */
+    public function down()
+    {
+        Schema::table('role_user', function (Blueprint $table) {
+            $table->dropForeign(['team_id']);
+        });
+
+        Schema::table('permission_user', function (Blueprint $table) {
+            $table->dropForeign(['team_id']);
+        });
+
+        Schema::dropIfExists('organizations');
+    }
+}
