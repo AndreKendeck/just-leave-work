3723a370483fac1c318e531c01f2d83171da96ab
Andre Kendeck <41024720+AndreKendeck@users.noreply.github.com>
2021-07-28 08:19:18 +0200

    WIP

--- app/Http/Controllers/Api/ImportUserController.php
+++ app/Http/Controllers/Api/ImportUserController.php
@@ -6,18 +6,15 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\User\ImportRequest;
 use App\Imports\UserImport;
 use Excel;
-use Exception;
 
 class ImportUserController extends Controller
 {
     public function import(ImportRequest $request)
     {
-        try {
-            $user = auth()->user();
-            Excel::import(new UserImport($user->team_id , $user->id), $request->users);
-        } catch (\Exception $e) {
-            return response()->json([ 'message' => 'Something went wrong']); 
-        }
+
+        $user = auth()->user();
+        Excel::import(new UserImport($user->team_id, $user->id), $request->users);
+
         return response()
             ->json([
                 'message' => "Import in progress, you will be notified when its complete",
